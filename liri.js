52adde0a8fa0af815b6878d199384a36d90263f5
golemheavy <steve@gridtop.com>
2019-04-30 16:52:20 -0500

    write out text in lines to log.txt, and then console.logs it as well -- default verbose mode

--- liri.js
+++ liri.js
@@ -1,16 +1,24 @@
-require("dotenv").config();
+if (require("dotenv").config()) writeMessage("loaded dotenv successfully");
+else writeMessage("failed to load dotenv");
+
 var keys = require("./keys.js");
+if (keys) writeMessage("loaded keys.js");
+else writeMessage("failed to load keys.js");
+
+
+function writeMessage(msgTextStr) {
+	const fs = require('fs');
+	fs.appendFile('log.txt', "\n" + msgTextStr, (err) => {  
+    if (err) throw err;
+    console.log(msgTextStr);
+});
+}
+
+
+
+
+
 // var spotify = new Spotify(keys[spotify]);
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 //
 //
 //Make it so liri.js can take in one of the following commands:

a6eb6e71adf08685b576d9fa7a29e275f2530359
golemheavy <steve@gridtop.com>
2019-04-30 18:47:09 -0500

    adding functionality to receive commang line commands

--- liri.js
+++ liri.js
@@ -1,5 +1,3 @@
-var inputString = process.argv;
-
 if (require("dotenv").config()) logmsg("loaded dotenv successfully");
 else logmsg("failed to load dotenv");
 
@@ -7,12 +5,22 @@ var keys = require("./keys.js");
 if (keys) logmsg("loaded keys.js");
 else logmsg("failed to load keys.js");
 
-switch(inputString[2]) {
-	case "concert-this"		: logmsg("Attempting CONCERT-THIS"); break; //Bands-In-Town
-	case "spotify-this-song": logmsg("Attempting SPOTIFY-THIS-SONG"); break; //Spotify
-	case "movie-this"		: logmsg("Attempting MOVIE-THIS"); break; // IMDB
-	case "do-what-it-says"	: logmsg("Attempting DO-WHAT-IT-SAYS"); break; //Random input
-	default: logmsg("please include an allowable argument as your first parameter"); return 0;
+logmsg("calling receiveCommandLineArguments()");
+receiveCommandLineArguments(process.argv);
+
+function receiveCommandLineArguments(strArr) {
+	switch(strArr[2]) {
+		case "concert-this"		: logmsg("Attempting CONCERT-THIS"); break; //Bands-In-Town
+		case "spotify-this-song": logmsg("Attempting SPOTIFY-THIS-SONG"); logmsg(spotifySong()); break; //Spotify
+		case "movie-this"		: logmsg("Attempting MOVIE-THIS"); break; // IMDB
+		case "do-what-it-says"	: logmsg("Attempting DO-WHAT-IT-SAYS"); break; //Random input
+		default: logmsg("please include an allowable argument as your first parameter"); return 0;
+	}
+}
+
+
+function spotifySong() {
+	return "spotifySong called";	
 }
 
 function logmsg(msgTextStr) {
@@ -20,10 +28,9 @@ function logmsg(msgTextStr) {
 	fs.appendFile('log.txt', "\n" + msgTextStr, (err) => {  
     if (err) throw err;
     console.log(msgTextStr);
-});
+	});
 }
 
 //var spotify = new Spotify(keys[spotify]);
-
 
 

13dcacc344f9f8505b51cb120af375d7af00b30c
golemheavy <steve@gridtop.com>
2019-05-03 23:08:42 -0500

    added some more tests and expected outputs for the defined test cases

--- tests/test-group-3-a.rb
+++ tests/test-group-3-a.rb
@@ -2,7 +2,7 @@ require "rubygems"
 gem "test-unit"
 require "test/unit"
 
-class TC_testgroup3a < Test::Unit::TestCase
+class TC_good_input_command_line < Test::Unit::TestCase
   # def setup
   # end
 
@@ -33,6 +33,48 @@ class TC_testgroup3a < Test::Unit::TestCase
   def test_05
 	_varActual = `node ../liri.js movie-this The Passion Of The Christ`
 	_varExpected = File.read("./expectedResults/expout5.txt")
+    assert_equal(_varExpected, _varActual, "The output didn't match expected output.")
+  end
+
+  # def teardown
+  # end
+
+end
+
+class TC_bad_input_command_line < Test::Unit::TestCase
+  # def setup
+  # end
+
+  def test_06
+	_varActual = `node ../liri.js movie-this jklkno`
+	_varExpected = File.read("./expectedResults/expout6.txt")
+    assert_equal(_varExpected, _varActual, "The output didn't match expected output.")
+  end
+  
+  def test_07
+	_varActual = `node ../liri.js movie-this E5,df5`
+	_varExpected = File.read("./expectedResults/expout7.txt")
+    assert_equal(_varExpected, _varActual, "The output didn't match expected output.")
+  end
+  
+  def test_08
+	_varActual = `node ../liri.js movie-this %34534`
+	_varExpected = File.read("./expectedResults/expout8.txt")
+    assert_equal(_varExpected, _varActual, "The output didn't match expected output.")
+  end
+
+  # def teardown
+  # end
+
+end
+
+class TC_no_input_command_line < Test::Unit::TestCase
+  # def setup
+  # end
+
+  def test_09
+	_varActual = `node ../liri.js movie-this`
+	_varExpected = File.read("./expectedResults/expout9.txt")
     assert_equal(_varExpected, _varActual, "The output didn't match expected output.")
   end
 
